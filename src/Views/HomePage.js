628ffd3c249ef997b858cee85778ed1e6e13a001
yassir soukaki <yassirsoukaki@gmail.com>
2025-04-12 16:57:42 -0400

    icon name

--- src/Views/HomePage.js
+++ src/Views/HomePage.js
@@ -12,7 +12,7 @@ const HomePage = () => {
     return (
         <div className="cafe-homepage">
             <h1>Welcome to Webcafe AI</h1>
-            <p>Your go-to solution for AI-driven web applications    .</p>
+            <p>Your go-to solution for AI-driven web applications hhhhh   .</p>
             <div className={`cafe-color-box ${isBlue ? 'cafe-blue' : 'cafe-red'}`}></div>
             <button className="cafe-button" onClick={toggleColor}>Change Color</button>
         </div>
